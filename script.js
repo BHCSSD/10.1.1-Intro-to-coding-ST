05580db476e7ecb6be8a86a8e3049f9347c4c4c4
BHCSSD <156448364+BHCSSD@users.noreply.github.com>
2024-01-27 17:41:27 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function setup() {
+  createCanvas(5,5);
+  
+}
+
+function draw() {
+  background(0);// change this colour... RGB 
+
+  
+//P5.js is what is known as a _________ of Javascript.
+//it was written to work with a ‘canvas’ to make ________, animating, and interactions easier.
+// x pixels wide, y pixels deep
+
+
+//It is common practice to end most most lines of code with a _____________
+
+
+
+//Everything is _______ sensitive. 
+// _______ is not the same as _________
+// with two words the first word is ______ and the second word is ______  jamesBond
+
+
+
+// there are 3 main areas for our basic codes
+//1.
+//2. 
+//3. 
+
+
+
+  // We can use _______ to help debug
+  // it is supper dupper hidden... click on the _______
+
+
+  // for now we will use 2 data types
+  // _______
+  // _______
+
+
+  
+  
+}
